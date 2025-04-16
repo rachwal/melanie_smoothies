5078ab7394b4421f985ab1a947405eac9c9b2794
Bartosz Rachwał <bartosz.rachwal@gmail.com>
2025-04-16 15:28:43 +0200

    json fix

--- streamlit-apps/custom-smoothie-order-form/streamlit_app.py
+++ streamlit-apps/custom-smoothie-order-form/streamlit_app.py
@@ -25,7 +25,7 @@ ingridients_list = st.multiselect(
 )
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response, use_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 if ingridients_list:
     
