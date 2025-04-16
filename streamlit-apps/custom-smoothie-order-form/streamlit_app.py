7fd9a3c0467821245c03cfcecca7a7e88a445cd2
Bartosz Rachwał <bartosz.rachwal@gmail.com>
2025-04-16 16:39:56 +0200

    search on

--- streamlit-apps/custom-smoothie-order-form/streamlit_app.py
+++ streamlit-apps/custom-smoothie-order-form/streamlit_app.py
@@ -32,11 +32,9 @@ if ingridients_list:
     for fruit_chosen in ingridients_list:
         ingredients_string += fruit_chosen + " "
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         pd_df = my_dataframe.to_pandas()
-
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         
     st.write(ingredients_string)
 
