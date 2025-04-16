03c2afa57e3e8eab544e21fc03daf540c0ce1023
Bartosz Rachwał <bartosz.rachwal@gmail.com>
2025-04-16 16:13:55 +0200

    add pandas

--- streamlit-apps/custom-smoothie-order-form/streamlit_app.py
+++ streamlit-apps/custom-smoothie-order-form/streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(f"Customize Your Smoothie! :cup_with_straw:")
@@ -16,7 +17,7 @@ st.write("The name on your Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"),col("SEARCH_ON")).collect()
 # st.dataframe(data=my_dataframe, use_container_width=True)
 ingridients_list = st.multiselect(
     'Choose up to 5 ingridients:',
@@ -33,6 +34,10 @@ if ingridients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        pd_df = sf_df.to_pandas()
+        st.write(pd_df)
+        st.stop();
+        
     st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
